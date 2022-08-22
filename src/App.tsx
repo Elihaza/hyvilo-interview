eb87ea61537a3398f70bb8538e26da788bd0c5f9
Elihaza <elias.larbi@gmail.com>
2022-08-23 01:14:07 +0200

    dynamically adding new contacts, need to dynamic add markers + fix empty localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import './App.css';
 import React, { useRef, useEffect, useState } from 'react';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
-import { MapProvider , Map, Marker} from 'react-map-gl';
-import { setMaxListeners } from 'events';
-import { createOptimisticUniqueName } from 'typescript';
+import { MapProvider , Map, Marker, Popup} from 'react-map-gl';
+// import { setMaxListeners } from 'events';
+// import { createOptimisticUniqueName, sortAndDeduplicateDiagnostics } from 'typescript';
 
 // mapboxgl.accessToken = 'pk.eyJ1IjoiZWxpaGF6YSIsImEiOiJjbDc0cnR5NjMwODcxM29wNnNqaGk1MnB3In0.O2YDd_RJn0jlC6izyWfCjA';
 
@@ -19,17 +19,18 @@ function Fiche(props: {name: string}) {
 
   return (
     <div className="ficheContact">
-        <h1>Fiche de {tab[index].name}</h1>
+        <h1>Fiche de {tab[index]?.name}</h1>
         <div className="leContact">
-          <p><strong>Nom : </strong>{tab[index].name}</p>
-          <p><strong>Descr. : </strong>{tab[index].description}</p>
-          <p><strong>Type : </strong>{tab[index].type}</p>
+          <p><strong>Nom : </strong>{tab[index]?.name}</p>
+          <p><strong>Descr. : </strong>{tab[index]?.description}</p>
+          <p><strong>Type : </strong>{tab[index]?.type}</p>
         </div>
       </div>
   )
 }
 
 function Contacts(props: {name: string}) {
+  const tab : {name: string, description: string, type: string}[] = JSON.parse(localStorage.getItem('my_array') || '[]');
   const [contacts, setContacts] = useState<{name: string}[]>([
     {name: 'Prupe Corp.'}, {name: 'Michel'}, {name: 'Bizières sur Zoid'}],);
 
@@ -40,16 +41,23 @@ function Contacts(props: {name: string}) {
   ]);
   
   const [currentContact, setCurrentContact] = useState<string>("");
-  const [show, setShow] = useState<boolean>(false);
-  const [index, setIndex] = useState<number>(0);
+  // const [show, setShow] = useState<boolean>(false);
+  // const [index, setIndex] = useState<number>(0);
+  const [leNom, setLeNom] = useState<string>(props.name);
 
-  localStorage.setItem('my_array', JSON.stringify(infoContact));
+  useEffect(() => {
+    localStorage.setItem('my_array', JSON.stringify(infoContact));
+    setLeNom(props.name);
+  }, [props.name]);
+
+  
+
 
   const handleClick = (name: string) => {
-    setShow(true);
+    setLeNom('');
+    // setShow(true);
     setCurrentContact(name);
-    console.log(currentContact);
-    setIndex(contacts.findIndex(contact => contact.name === name));
+    // setIndex(contacts.findIndex(contact => contact.name === name));
   }
 
   return (
@@ -58,99 +66,39 @@ function Contacts(props: {name: string}) {
         <h1>Contacts</h1>
       </div>
       <div className="Contact">
-        {contacts.map((contact, index) => (
+        {tab?.map((contact, index) => (
           <div onClick={() => handleClick(contact.name)} className="Personne" key={index}>
-            <p>{contact.name}</p>
+            <p><strong>{contact.name}</strong></p>
           </div>
         ))}
       </div>
-      {props.name ? <Fiche name={props.name} />
-        :  <Fiche name={currentContact}/>}
+      {leNom ? <Fiche name={leNom}/> : <Fiche name={currentContact}/>}
     </div>
   );
 }
 
 
 function App() {
-  // const {current: myMap} = useMap();
-  // const [lng, setLng] = useState<number>(2);
-  // const [lat, setLat] = useState<number>(46.95);
-
-  // const handleClick = (e: mapboxgl.MapLayerMouseEvent) => {
-  //   setLng(e.lngLat.lng);
-  //   setLat(e.lngLat.lat);
-  //   console.log('tronchw');
-  //   myMap?.flyTo({center: [4, 40]});
-  // }
-
-  
-
-  // return (
-  //   <div className="App">
-  //     <MapProvider>
-  //       <Map
-  //         id="myMap"
-  //         onClick={(e) => {handleClick(e)}}
-  //         mapboxAccessToken={MAPBOX_TOKEN}
-  //         initialViewState={{
-  //           longitude: lng,
-  //           latitude: lat,
-  //           zoom: 5.4,
-  //         }}
-  //         mapStyle="mapbox://styles/mapbox/streets-v11"
-  //       >
-  //       </Map>
-  //     </MapProvider>
-  //     <Contacts />
-  //   </div>
-  // );
-  
-  // const mapContainer = useRef<HTMLDivElement>(null);
-  // const map = useRef<Map | null>(null);
-  // const [myMap, setMyMap] = useState<Map | null>(null);
   const [lng, setLng] = useState<number>(2);
   const [lat, setLat] = useState<number>(46.95);
-  const [show, setShow] = useState<boolean>(false);
   const [currentContact, setCurrentContact] = useState<string>("");
   const tab : {name: string, description: string, type: string}[] = JSON.parse(localStorage.getItem('my_array') || '[]');
-  // const [zoom, setZoom] = useState<number>(5.4);
+  const [showPopup, setShowPopup] = React.useState(false);
 
-  // const marker = new mapboxgl.Marker()
-  //   .setLngLat([lng, lat])
-  //   .addTo(myMap);
-  
-  // useEffect(() => {
-  //   if (map.current) return; // initialize map only once 
-  //   map.current = new Map({
-  //     container: mapContainer.current || '',
-  //     style: 'mapbox://styles/mapbox/streets-v11',
-  //     center: [lng, lat],
-  //     zoom: zoom
-  //   });
-
-  //   setMyMap(map.current);
-
-  //   const markerParis = new mapboxgl.Marker()
-  //   .setLngLat([2.33, 48.86])
-  //   .addTo(map.current);
-  //   const markerMichel = new mapboxgl.Marker()
-  //   .setLngLat([4.8, 45.7])
-  //   .addTo(map.current);
-  //   const markerBizieres = new mapboxgl.Marker()
-  //   .setLngLat([1.9, 47.903])
-  //   .addTo(map.current);
-  //   map.current.on('click', (e) => {});
-  // });
+  const [newContactName, setNewContactName] = useState<string>("");
+  const [newContactDescription, setNewContactDescription] = useState<string>("");
+  const [newContactType, setNewContactType] = useState<string>("Entreprise");
 
   const handleClick = (name: string) => {
-    setShow(true);
+    // setShow(true);
+    setShowPopup(true);
     setCurrentContact(name);
-    console.log(currentContact);
   }
 
   const createNewMarker = (e: mapboxgl.MapLayerMouseEvent) => {
     setLng(e.lngLat.lng);
     setLat(e.lngLat.lat);
+    setShowPopup(showPopup => !showPopup);
   }
 
   return (
@@ -168,11 +116,48 @@ function App() {
       <Marker onClick={() => handleClick('Prupe Corp.')} longitude={2.33} latitude={48.86}/>
       <Marker onClick={() => handleClick('Michel')} longitude={4.8} latitude={45.7}/>
       <Marker onClick={() => handleClick('Bizières sur Zoid')} longitude={1.9} latitude={47.903}/>
+      {showPopup && (
+      <Popup longitude={lng} latitude={lat} anchor="bottom" onClose={() => {setNewContactName(''); setNewContactDescription(''); setNewContactType('');}}>
+        <div className="titrePopup"><h2>Ajouter nouveau contact</h2></div>
+        <div className="allInputs">
+          <input className="inputName"
+            type="text"
+            placeholder='Nom'
+            value={newContactName}
+            onChange={(e) => setNewContactName(e.target.value)}
+          />
+          <textarea className="inputDesc"
+            rows={5}
+            cols={21}
+            placeholder='Description'
+            value={newContactDescription}
+            onChange={(e) => setNewContactDescription(e.target.value)}
+          />
+          <form className="inputType">
+            <strong>Séléctionner le type de contact :</strong>
+            <select value={newContactType} onChange={(e) => setNewContactType(e.target.value)}>
+              <option value="Entreprise">Entreprise</option>
+              <option value="Particulier">Particulier</option>
+              <option value="Collectivité">Collectivité</option>
+            </select>
+            <button className="btnAdd" onClick={(e) => {
+              e.preventDefault();
+              const newTab = JSON.stringify([...tab, {name: newContactName, description: newContactDescription, type: newContactType}]);
+              localStorage.setItem('my_array', newTab);
+              setShowPopup(showPopup => !showPopup);
+            }
+            }>Ajouter</button>
+        </form> 
+      </div>
+      </Popup>)}
       </Map>
+      
     </div>
     {lng} {lat}
+    
     {currentContact ? <Contacts name={currentContact}/> : <Contacts name={'Prupe Corp.'}/>}
     {/* <div className="contacts">{show && <Fiche name={currentContact}/>}</div> */}
+    
   </div>
   );
 }
